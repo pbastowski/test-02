77ed7024e2db77583bdd4db4fd0e5db01a9ed72e
Paul Bastowski <pbastowski@gmail.com>
2016-01-03 19:47:04 +0100

    Initial commit

---
+++ app.ts
@@ -0,0 +1,17 @@
+import { SetModule, Component, Inject, bootstrap } from 'angular2now';
+
+SetModule('app', []);
+
+@Component({
+    selector: 'app',
+    template: '<h1>angular2-now Application</h1>',
+    services: ['$http']
+})
+@Inject('$http')
+class App {
+    constructor($http) {
+        console.log('@ app: $http: ', $http);
+    }
+}
+
+bootstrap(App);
