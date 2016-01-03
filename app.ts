ba6c257406926e11b77da8a3ef07e8607949e9c6
Paul Bastowski <pbastowski@gmail.com>
2016-01-03 19:56:33 +0100

    Some cleanup.

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ SetModule('app', []);
     template: '<h1>angular2-now Application</h1>',
     services: ['$http']
 })
-@Inject('$http')
+//@Inject('$http')
 class App {
     constructor($http) {
         console.log('@ app: $http: ', $http);
